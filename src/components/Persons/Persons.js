173a4420e1ccaaf740e846e217698e496b4d230f
Nachiket75 <nachinatekar@gmail.com>
2020-11-24 15:21:09 +0530

    Component update life cycle (for prop changes)

--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -1,7 +1,36 @@
 import React,{Component} from 'react'
 import Person from './Person/Person'
 
-class Persons extends Component { //const persons = (props) =>                    
+class Persons extends Component { 
+  // static getDerivedStateFromProps(props, state) {
+  //   console.log('[Persons.js] getDerivedStateFromProps');
+  //   return state;
+  // }
+
+  // componentWillReceiveProps(props) {
+  //   console.log('[Persons.js] componentWillReceiveProps', props);
+  // }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log('[Persons.js] shouldComponentUpdate');
+    return true;
+  }
+
+  getSnapshotBeforeUpdate(prevProps, prevState) {
+    console.log('[Persons.js] getSnapshotBeforeUpdate');
+    return { message: 'Snapshot!' };
+  }
+
+  // componentWillUpdate() {
+
+  // }
+
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    console.log('[Persons.js] componentDidUpdate');
+    console.log(snapshot);
+  }
+
+  
   render(){
     console.log("[Persons.js] is rendering...");
     return this.props.persons.map((person,index)=> {
