a7742ab76c7f120a720e3802f59a68ea714389f3
Nachiket75 <nachinatekar@gmail.com>
2020-11-26 15:45:26 +0530

    cleanup to create new burger project.

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class App extends Component {  
+
+  render() {      
+    return ( 
+      <div>
+         Hello        
+      </div>      
+    )
+  }
+}
+
+export default App;
+
+//add Radium above App is called higher order component i.e wrapping you component and adding some extra functionality 
