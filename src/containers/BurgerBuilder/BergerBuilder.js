8f211b015573a1b6c4c657ad606deca4abfcfbed
Nachiket75 <nachinatekar@gmail.com>
2020-11-28 15:49:38 +0530

    Implemented Less button functionality to remove added ingredient.
    Also added logic to disable LESS button if no ingredients added of particular type.

--- src/containers/BurgerBuilder/BergerBuilder.js
+++ src/containers/BurgerBuilder/BergerBuilder.js
@@ -37,15 +37,40 @@ class BergerBuilder extends Component{
     }
     
     removeIngredientHandler= (type)=> {
+        const oldCount = this.state.ingredients[type];
+        if(oldCount<=0){                                 //if oldCount is zero we wont subtract more as already it is 0 and ingredients added count can not be negative.
+            return;
+        }
+
+        const updateCount = oldCount-1;
+        const updatedIngredients = {
+            ...this.state.ingredients
+        }
+        updatedIngredients[type] = updateCount;
+        const priceDeduction = INGREDIENT_PRICES[type];
+        const oldPrice = this.state.totalPrice;
+        const newPrice = oldPrice - priceDeduction;
+
+        this.setState({totalPrice:newPrice, ingredients:updatedIngredients})
+        
+        console.log(newPrice);
 
     }
 
     render(){
+        const disabledInfo = {
+            ...this.state.ingredients
+        }
+        for(let key in disabledInfo){
+            disabledInfo[key]=disabledInfo[key]<=0;         //this will keep true and false status if ingredient count is 0 then to disabled the less button.
+        }
         return(
             <Aux>
                 <Burger ingredients={this.state.ingredients}/>
                 <BuildControls
-                    ingredientAdded = {this.addIngredientHandler}/>
+                    ingredientAdded = {this.addIngredientHandler}
+                    ingredientRemoved = {this.removeIngredientHandler}
+                    disabled={disabledInfo}/>
             </Aux>
         )
     }
