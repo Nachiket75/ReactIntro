7d8bb0e731d11385f971eefc40cb18012a456b40
Nachiket75 <nachinatekar@gmail.com>
2020-12-02 16:24:29 +0530

    implemented SideDrawerToggle for mobile resolution which will pop up when screen resoltion goes below 500.
    converted Layout.js to class based component to add new fucntions which will handle SideDrawer.

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,20 +1,35 @@
-import React from 'react'
+import React,{Component} from 'react'
 import Aux from '../../hoc/Auxilary'
 import classes from './Layout.css'
 import Toolbar from '../Navigation/Toolbar/Toolbar'
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer'
-const layout =(props) => (
-    <Aux>
-        <Toolbar/>
-        <SideDrawer/>
-        <div>           
-            SideDrawer,
-            Backdrop
-        </div>
-        <main className= {classes.content}>
-            {props.children}
-        </main>
-    </Aux>
-)
 
-export default layout;
+class Layout extends Component {
+    state = {
+        showSideDrawer : false
+    }
+    sideDrawerCloseHandler=()=> {
+        this.setState({showSideDrawer:false})
+    }
+    sideDrawerToggleHandler=()=>{
+        this.setState((prevState) =>{
+        return {showSideDrawer:!prevState.showSideDrawer}
+        })
+    }
+    render(){
+        return(
+            <Aux>
+                <Toolbar drawerToggleClicked ={this.sideDrawerToggleHandler}/>
+                <SideDrawer 
+                    open={this.state.showSideDrawer} 
+                    closed={this.sideDrawerCloseHandler}
+                    />                
+                <main className= {classes.content}>
+                    {this.props.children}
+                </main>
+            </Aux>
+        )
+    }
+}
+
+export default Layout;
--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -2,9 +2,10 @@ import React from 'react'
 import classes from './Toolbar.css'
 import Logo from '../../Logo/Logo'
 import NavigationItems from '../NavigationItems/NavigationItems'
+import SideDrawerToggle from '../SideDrawer/SideDrawerToggle/SideDrawerToggle'
 const toolbar = (props) =>(
     <header className={classes.Toolbar}>
-        <div>MENU</div>
+        <SideDrawerToggle clicked={props.drawerToggleClicked}/>
         <div className={classes.Logo}>
             <Logo/>
         </div>            
