8ed631aa6bbcb5d34c6f204bf1498f040aa023e4
Nachiket75 <nachinatekar@gmail.com>
2020-12-03 13:14:14 +0530

    starting from scratch

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import React, { Component } from 'react';
-import Layout from './components/Layout/Layout'
-import BurgerBuilder from './containers/BurgerBuilder/BergerBuilder'
+
 class App extends Component {  
   render() {      
     return ( 
       <div>
-        <Layout>
-          <BurgerBuilder/>
-        </Layout>               
+                 
       </div>      
     )
   }
