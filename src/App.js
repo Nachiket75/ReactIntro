b5e0a7ce3e2878dd967aba62c302979e37596bf1
Nachiket75 <nachinatekar@gmail.com>
2020-12-03 16:54:14 +0530

    Addedl container

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import Blog from './containers/Blog/Blog'
 
 class App extends Component {  
   render() {      
     return ( 
       <div>
-                 
+        <Blog />   
       </div>      
     )
   }
---
+++ src/containers/Blog/Blog.js
@@ -0,0 +1,22 @@
+import React,{Component} from 'react'
+import  './Blog.css'
+class Blog extends Component{
+    render(){
+        return(
+            <div >
+                <section className="posts">
+                    Post
+                </section>
+                <section>
+                    Full Post
+                </section>
+                <section>
+                    New Post
+                </section>
+
+            </div>
+        )
+    }
+}
+
+export default Blog;
