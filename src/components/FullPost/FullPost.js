673e20b1ec8c9808200546e329d99885ab8ef4ed
Nachiket75 <nachinatekar@gmail.com>
2020-12-08 17:35:33 +0530

    loaded full post which user selected .Display title and  body of user selected post using id property
    It has one error if you go to network tab you will see continous sending of request (infinite request) because we are upading
    state from componentDidUpdate() method. we call setState in componentDidUpdate() which update component and called componentDidUpdate() method in this again setState() method will call and update the component and it goes on.

--- src/components/FullPost/FullPost.js
+++ src/components/FullPost/FullPost.js
@@ -1,14 +1,31 @@
 import React,{Component} from 'react'
 import classes from './FullPost.css'
+import axios from 'axios';
 
 class FullPost extends Component{
+    state = {
+        loadedPost:null
+    }
+    componentDidUpdate(){
+        if(this.props.id){
+            axios.get('https://jsonplaceholder.typicode.com/posts/'+this.props.id)
+            .then(response=>{
+                this.setState({loadedPost : response.data})
+                //console.log(response)
+            })
+        }
+        
+    }
     render(){
         let post = <p style={{textAlign:"center"}}>Please Select a Post!</p>;
         if(this.props.id){
+            post = <p style={{textAlign:"center"}}>Loading...</p>;
+        }
+        if(this.state.loadedPost ){
             post=(
                 <div className={classes.FullPost}>
-                    <h1>Title</h1>
-                    <p>Content</p>
+                    <h1>{this.state.loadedPost.title}</h1>
+                    <p>{this.state.loadedPost.body}</p>
                     <div className={classes.Edit}>
                         <button className={classes.Delete}>Delete</button>
                     </div>
