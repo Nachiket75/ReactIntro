0156b1a1310dc56a93edc12817cd54e9ac84694d
Nachiket75 <nachinatekar@gmail.com>
2020-12-16 16:07:33 +0530

    cleanup for assignment 3

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
 import React, { Component } from 'react';
-import Blog from './containers/Blog/Blog';
-import {BrowserRouter} from 'react-router-dom'; //BrowserRouter is used to do routing to make single page application look like multiple page application
 
 class App extends Component {  
   render() {      
-    return ( 
-      <BrowserRouter>      
+    return (       
         <div>
-          <Blog />   
+   
         </div>      
-      </BrowserRouter>
     )
   }
 }
 
 export default App;
-
-//we are using https://jsonplaceholder.typicode.com/ for GET POSTS and all RESTFUL requests.
-
-//we are using axios package to make RESTFUL http request easily. use npm install axios --save on terminal
-// to save this package in npm so that we can use it.
