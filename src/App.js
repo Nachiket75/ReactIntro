23d048bcd21aa313d4126560624b602effabeefa
Nachiket75 <nachinatekar@gmail.com>
2020-12-18 20:31:22 +0530

    Created checkout-summary for routing to show this page when user click order now.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import Layout from './components/Layout/Layout'
 import BurgerBuilder from './containers/BurgerBuilder/BergerBuilder'
+import Checkout from './containers/Checkout/Checkout'
 class App extends Component {  
   render() {      
     return ( 
       <div>
         <Layout>
           <BurgerBuilder/>
+          <Checkout/>
         </Layout>               
       </div>      
     )
