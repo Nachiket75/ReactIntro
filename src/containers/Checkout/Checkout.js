fa8c9141a17cd46a81de9859e6713abfaee33551
Nachiket75 <nachinatekar@gmail.com>
2020-12-19 15:56:21 +0530

    configured Checkout page cancel and continue button to go back and forth.
    we only added route /checkout/contact-data we still need to implement that route.

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -10,10 +10,21 @@ class Checkout extends Component{
 
         }
     }
+
+    checkoutCancelledHandler = () =>{
+        this.props.history.goBack()               //goBack method will go to previous page on the stack
+    }
+
+    checkoutContinueHandler = () =>{
+        this.props.history.replace('/checkout/contact-data')     //replace method will top of the stack with checkout url and you will go to checkout page
+    }
+
     render(){
         return(
             <div>
-                <CheckoutSummary ingredients = {this.state.ingredients}/>
+                <CheckoutSummary ingredients = {this.state.ingredients}
+                checkoutContinue = {this.checkoutContinueHandler}
+                checkoutCancelled ={this.checkoutCancelledHandler} />
             </div>
         )
     }
