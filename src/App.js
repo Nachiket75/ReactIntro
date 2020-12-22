1703f1088b97cdcf3a7ff9b073fe490adf956d28
Nachiket75 <nachinatekar@gmail.com>
2020-12-22 18:15:52 +0530

    Added Orders.js  to show user orders.

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import Layout from './components/Layout/Layout'
 import BurgerBuilder from './containers/BurgerBuilder/BergerBuilder'
 import Checkout from './containers/Checkout/Checkout'
 import {Route,Switch} from 'react-router-dom'
+import Orders from './containers/Orders/Orders'
 class App extends Component {  
   render() {      
     return ( 
       <div>
         <Layout>
           <Switch>
-            <Route path="/checkout"  component={Checkout}/>           
+            <Route path="/checkout"  component={Checkout}/>     
+            <Route path="/orders" component={Orders}/>
             <Route path="/" exact component={BurgerBuilder} />           
           </Switch>
         </Layout>               
---
+++ src/components/Order/Order.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import classes from './Order.css'
+const order = (props) => (
+    <div className={classes.Order}>
+        <p>Ingredients: Salad(1)</p>
+        <p><strong>USD 4.5</strong></p>
+    </div>
+)
+
+export default order;
