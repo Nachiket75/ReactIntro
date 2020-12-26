d13d2ad005e643b626b2b543b74e4a4560fa065c
Nachiket75 <nachinatekar@gmail.com>
2020-12-26 18:02:52 +0530

    cleaup for section 14 redux

--- src/axios-orders.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios'
-
-const instance = axios.create({
-    baseURL:'https://react-burger-331dd-default-rtdb.firebaseio.com/' //This is firebase realtime database url
-})
-
-export default instance;
