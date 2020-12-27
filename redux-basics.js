3a421b96c329cd6a34018295a5991719d21a6ce6
Nachiket75 <nachinatekar@gmail.com>
2020-12-27 15:23:13 +0530

    use of redux Dispatching actions and subscription

--- redux-basics.js
+++ redux-basics.js
@@ -8,11 +8,34 @@ const initialState={
 
 //Reducer
 const rootReducer = (state=initialState, action)=>{
+    if(action.type === 'INC_COUNTER'){
+        return{
+            ...state,
+            counter:state.counter + 1
+        }
+    }
+    if(action.type === 'ADD_COUNTER'){
+        return{
+            ...state,
+            counter:state.counter + action.value
+        }
+    }
     return state
 }
 
 //store
 const store = createStore(rootReducer);
+console.log(store.getState())
+
+//subscribe to future dispatch action when dispatch action called and state changes it will trigger and show state output
+store.subscribe(()=>{
+    console.log('Subscription',store.getState())
+    });
+
+//Dispathing Action
+store.dispatch({type:'INC_COUNTER'});
+store.dispatch({type:'ADD_COUNTER',value:10})
+
 console.log(store.getState())
 
 
