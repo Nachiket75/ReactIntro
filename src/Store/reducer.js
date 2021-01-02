d4a2cba50c670d270fc031b1e683577de9bd8632
Nachiket75 <nachinatekar@gmail.com>
2021-01-02 18:08:49 +0530

    Basic Redux setup to apply redux in Burger project

---
+++ src/Store/reducer.js
@@ -0,0 +1,12 @@
+import * as actionTypes from './actionTypes'
+
+initialState = {
+    ingredients:{},      
+    totalPrice:0
+}
+
+const reducer = (state = initialState, actions) =>{
+
+}
+
+export default reducer;
