aa0cb61ae8e6b9aa3140cabedcd43044114b7a40
Nachiket75 <nachinatekar@gmail.com>
2021-01-05 14:52:34 +0530

    search redux devtools in google install chrome package  and visit link https://github.com/zalmoxisus/redux-devtools-extension you can find composeEnhancers code their and its use.You can access chrome package from browser go to inspect press arrow and click redux.
    you can go to previous any state just by clicking that state

--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import {createStore, combineReducers,applyMiddleware} from 'redux'
+import {createStore, combineReducers,applyMiddleware,compose} from 'redux'
 import counterReducer from './Store/reducers/counter'
 import resultReducer from './Store/reducers/result'
 import {Provider} from 'react-redux'
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;     //this is advance redux can be found on the link https://github.com/zalmoxisus/redux-devtools-extension or by searching redux-dev tools
 
 const logger = store => {
     return next =>{
@@ -24,6 +26,6 @@ const rootReducer = combineReducers({
     res:resultReducer
 })
 
-const store = createStore(rootReducer,applyMiddleware(logger))  
+const store = createStore(rootReducer,composeEnhancers(applyMiddleware(logger)))  
 ReactDOM.render(<Provider store={store}><App/></Provider> , document.getElementById('root'));
 registerServiceWorker();
