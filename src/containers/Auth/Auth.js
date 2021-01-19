7136f58f311384a46b517b7b42c0592dc7b5fa2c
Nachiket75 <nachinatekar@gmail.com>
2021-01-19 19:35:51 +0530

    display error message as error above username field

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -5,6 +5,7 @@ import classes from './Auth.css'
 import * as actionCreators from '../../Store/actions/actionCreator'
 import {connect} from 'react-redux'
 
+
 class Auth extends Component{
     state ={
         isSignUp:true
@@ -20,12 +21,19 @@ class Auth extends Component{
         this.setState(prevState =>{
             return {isSignUp:!prevState.isSignUp}
         })
-    }
+    }    
 
-    render(){
+    render(){    
+        let errormessage = null;
+        if(this.props.error){
+            errormessage = (
+                <p>{this.props.error.message}</p>
+            )
+        }
         return(
             <div className={classes.Auth}>
                 <h1>Sign In</h1>
+                {errormessage}
                 <form onSubmit={this.signInHandler}>                    
                     <Input inputtype="input" label="username" type="text" id="username" placeholder="Username" minlen={5}/>
                     <Input inputtype="input" label="password" type="password" id="password" placeholder="Password" minlen={7}/>
@@ -42,6 +50,11 @@ class Auth extends Component{
     }
 }
 
+const mapStateToProps = state =>{
+    return{        
+        error:state.auth.error
+    }
+}
 const mapDispatchToProps = dispatch =>{
     return{
         onAuth: (email,password,isSignUp)=> dispatch(actionCreators.auth(email,password,isSignUp))
@@ -49,4 +62,4 @@ const mapDispatchToProps = dispatch =>{
 
 }
 
-export default connect(null,mapDispatchToProps) (Auth);
+export default connect(mapStateToProps,mapDispatchToProps) (Auth);
