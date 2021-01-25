ba822aead5258561f631b222edda4c26ffe9d59d
Nachiket75 <nachinatekar@gmail.com>
2021-01-25 11:25:27 +0530

    UI changes Route and navigation item changed to  Logout from Authenticate after user authenticated successfully,

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -3,6 +3,7 @@ import Aux from '../../hoc/Auxilary'
 import classes from './Layout.css'
 import Toolbar from '../Navigation/Toolbar/Toolbar'
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer'
+import {connect} from 'react-redux'
 
 class Layout extends Component {
     state = {
@@ -19,8 +20,11 @@ class Layout extends Component {
     render(){
         return(
             <Aux>
-                <Toolbar drawerToggleClicked ={this.sideDrawerToggleHandler}/>
+                <Toolbar 
+                    isAuth = {this.props.isAuthenticated}
+                    drawerToggleClicked ={this.sideDrawerToggleHandler}/>
                 <SideDrawer 
+                    isAuth = {this.props.isAuthenticated}
                     open={this.state.showSideDrawer} 
                     closed={this.sideDrawerCloseHandler}
                     />                
@@ -32,4 +36,9 @@ class Layout extends Component {
     }
 }
 
-export default Layout;
+const mapStateToProps = state =>{
+    return{
+        isAuthenticated: state.auth.token!=null
+    }
+}
+export default connect(mapStateToProps)(Layout);
