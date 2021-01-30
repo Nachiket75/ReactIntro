5ceecf72d9a98884dbc3899dc5fc00d34904533a
Nachiket75 <nachinatekar@gmail.com>
2021-01-30 21:03:24 +0530

    Gurading routes to only display some routes when user is autenticated and when user in not authenticated show different routes

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import Layout from './components/Layout/Layout'
 import BurgerBuilder from './containers/BurgerBuilder/BergerBuilder'
 import Checkout from './containers/Checkout/Checkout'
-import {Route,Switch} from 'react-router-dom'
+import {Route,Switch,Redirect} from 'react-router-dom'
 import Orders from './containers/Orders/Orders'
 import Auth from './containers/Auth/Auth'
 import Logout from './containers/Logout/Logout'
@@ -13,20 +13,40 @@ class App extends Component {
   componentDidMount(){
     this.props.onTryAutoSignUp()
   }
-  render() {      
+  render() {     
+    let routes = (
+      <Switch>
+        <Route path="/auth" component={Auth}/>
+        <Route path="/" exact component={BurgerBuilder}/>
+        <Redirect to="/"/>
+      </Switch>
+
+    ) 
+
+    if(this.props.isAuthenticated){
+      routes = (
+        <Switch>
+          <Route path="/checkout"  component={Checkout}/>     
+          <Route path="/orders" component={Orders}/>         
+          <Route path="/logout" component ={Logout}/>
+          <Route path="/" exact component={BurgerBuilder} />           
+          <Redirect to="/"/>
+        </Switch>
+      )
+    }
     return ( 
       <div>
         <Layout>
-          <Switch>
-            <Route path="/checkout"  component={Checkout}/>     
-            <Route path="/orders" component={Orders}/>
-            <Route path="/auth" component={Auth}/>
-            <Route path="/logout" component ={Logout}/>
-            <Route path="/" exact component={BurgerBuilder} />           
-          </Switch>
+          {routes}
         </Layout>               
       </div>      
     )
+  }
+}
+
+const mapStateToProps = state=>{
+  return{
+    isAuthenticated: state.auth.token!= null
   }
 }
 
@@ -36,6 +56,6 @@ const mapDispatchToProps =dispatch=>{
   }
 }
 
-export default connect(null,mapDispatchToProps) (App);
+export default connect(mapStateToProps,mapDispatchToProps) (App);
 
 //add Radium above App is called higher order component i.e wrapping you component and adding some extra functionality 
